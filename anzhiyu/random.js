6fc8dcc842dbe18bcf8a1210aacb7bfb83671b57
Kajiekazz <719571357@qq.com>
2023-10-03 15:43:19 +0800

    Site updated: 2023-10-03 15:43:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/09/29/My-New-Post/","2023/09/29/hello-world/","2023/10/01/图床测试/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/09/29/My-New-Post/","2023/10/01/图床测试/","2023/10/03/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
